930df92e2b6891469486b169a3fe73348bf45b18
Yuji Nakamura <50049575+Yuji5117@users.noreply.github.com>
2020-04-10 01:05:19 +0900

    commit

--- contact.php
+++ contact.php
@@ -24,7 +24,7 @@ function escape($str) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="css/reset.css">
-    <link rel="stylesheet" href="css/contact.css">
+    <link rel="stylesheet" href="css/contact_form.css">
     <title>Contact us</title>
 </head>
 <body>
